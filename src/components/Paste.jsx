70412ef11e96c7b00e6caa21e66a464350f1e307
Rohan Singh <rohans212004@gmail.com>
2024-12-29 12:48:52 +0530

    paste changes

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Paste = () => {
+  const pastes = useSelector((state)=>state.paste.pastes);
+  const dispatch = useDispatch();
+
+  const filteredData = pastes.filter((paste)=>paste.title.toLowercase().includes(searchTerm.toLowercase()));
+    
+
   return (
     <div>
       
