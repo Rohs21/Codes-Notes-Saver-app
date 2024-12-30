5886253083a9c44fb2752deae2faaa83b6fc3fec
Rohan Singh <rohans212004@gmail.com>
2024-12-30 14:01:01 +0530

    Search bar

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 const Paste = () => {
   const pastes = useSelector((state)=>state.paste.pastes);
+  const [SearchTerm, setSearchTerm] = useState('');
   const dispatch = useDispatch();
 
-  const filteredData = pastes.filter((paste)=>paste.title.toLowercase().includes(searchTerm.toLowercase()));
+  const filteredData = pastes.filter(
+          (paste)=>paste.title.toLowerCase().
+          includes(SearchTerm.toLowerCase())
+        );
+
     
 
   return (
     <div>
+      <input
+      type='search'
       
+      />
     </div>
   )
 }
