99540f82d978e29a9522db0300607ecb41344537
Rohan Singh <rohans212004@gmail.com>
2025-01-14 11:18:01 +0530

    Updated Css and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,41 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import ViewPaste from './components/ViewPaste';
-import Paste from './components/Paste';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./components/Home"
+import Paste from "./components/Paste"
+import ViewPaste from "./components/ViewPaste"
+import Navbar from "./components/Navbar"
+
 
 const router = createBrowserRouter(
   [
     {
-    path:"/",
-    element:
-    <div>
-      <Navbar />
-      <Home />
+      path:"/",
+      element:
+      <div className="w-full h-full flex flex-col">
+        <Navbar/>
+        <Home/>
+      </div>
+    },
+    {
+      path:"/pastes",
+      element: <div className="w-full h-full flex flex-col">
+      <Navbar/>
+      <Paste/>
     </div>
     },
     {
-      path:"pastes",
-      element:
-      <div>
-        <Navbar />
-        <Paste />
-      </div>
-    },
-    {
-      path:"pastes/:id",
-      element:
-      <div>
-        <Navbar />
-        <ViewPaste />
-      </div>
-      },
-
+      path:"/pastes/:id",
+      element: <div className="w-full h-full flex flex-col">
+      <Navbar/>
+      <ViewPaste/>
+    </div>,
+    }
   ]
-);
+)
 
 function App() {
 
   return (
-      <div>
-      <RouterProvider router = {router}/>
-      </div>
+    <RouterProvider router={router}/>
   )
 }
 
--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -1,63 +1,70 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { toast } from 'react-hot-toast';
+import { createSlice } from "@reduxjs/toolkit"
+import { toast } from "react-hot-toast"
 
 const initialState = {
-    pastes:localStorage.getItem("pastes")
-    ? JASON.parse(localStorage.getItem("pastes"))
+  pastes: localStorage.getItem("pastes")
+    ? JSON.parse(localStorage.getItem("pastes"))
     : []
 }
 
 const pasteSlice = createSlice({
-  name: 'paste',
+  name: "paste",
   initialState,
   reducers: {
-    addToPaste: (state, action) => {
-     const paste = action.payload
-     state.pastes.push(paste);
-     localStorage.setItem("Pastes",
-     JSON.stringify(state.pastes));
-     toast("Notes Created Successfuly")
-    },
-
-    updateToPaste: (state, action) => {
+    addToPastes: (state, action) => {
       const paste = action.payload
-      const index = state.pastes.findIndex((item) =>
-      item._id === paste._id);
+      const index = state.pastes.findIndex((item) => item._id === paste._id)
 
-      if(index >= 0){
-        state.pastes[index] = paste;
-        localStorage.setItem("Pastes",
-        JSON.stringify(state.pastes))
-        toast.success("Notes Updated");
+      if (index >= 0) {
+        // If the course is already in the Pastes, do not modify the quantity
+        toast.error("Paste already exist")
+        return
       }
-
-    },
-
-    resetAllPaste: (state, action) => {
-      state.pastes= [];
-      localStorage.removeItem("pastes");
-    },
-
-    removeFromPaste: (state, action) => {
-      const pasteId = action.payload
-      console.log(pasteId);
-      const index = state.pastes.findIndex((item) =>
-      item._id === pasteId);
-      if(index >=0){
-        state.pastes.splice(index,1);
-
-        localStorage.setItem("Pastes",
-        JSON.stringify(state.pastes));
-
-        toast.success("Notes Deleted");
-      }
-
+      // If the course is not in the Pastes, add it to the Pastes
+      state.pastes.push(paste)
       
+      // Update to localstorage
+      localStorage.setItem("pastes", JSON.stringify(state.pastes))
+      // show toast
+      toast.success("Paste added")
+    },
+
+    updatePastes: (state, action) => {
+      const paste = action.payload
+      const index = state.pastes.findIndex((item) => item._id === paste._id)
+
+      if (index >= 0) {
+        // If the course is found in the Pastes, update it
+        state.pastes[index] = paste
+        // Update to localstorage
+        localStorage.setItem("pastes", JSON.stringify(state.pastes))
+        // show toast
+        toast.success("Paste updated")
+      }
+    },
+    removeFromPastes: (state, action) => {
+      const pasteId = action.payload
+
+      console.log(pasteId)
+      const index = state.pastes.findIndex((item) => item._id === pasteId)
+
+      if (index >= 0) {
+        // If the course is found in the Pastes, remove it
+        state.pastes.splice(index, 1)
+        // Update to localstorage
+        localStorage.setItem("pastes", JSON.stringify(state.pastes))
+        // show toast
+        toast.success("Paste deleted")
+      }
+    },
+    resetPaste: (state) => {
+      state.pastes = []
+      // Update to localstorage
+      localStorage.removeItem("pastes")
     },
   },
 })
 
-// Action creators are generated for each case reducer function
-export const { addToPaste, updateToPaste, resetAllPaste,removeFromPaste } = pasteSlice.actions
+export const { addToPastes, removeFromPastes, updatePastes } = pasteSlice.actions
 
 export default pasteSlice.reducer
